e58d7893ddcf03d49dd5e4ae1e9ca86ff2c6cfd2
TheStarFrost <138985340+TheStarFrost@users.noreply.github.com>
2024-08-26 15:00:02 +0800

    完成验收版本->踢球入门版本

--- Project/USER/main.c
+++ Project/USER/main.c
@@ -3,7 +3,7 @@
 @E-mail  ??chiusir@163.com
 @????IDE ??KEIL5.25.3???????
 @'??????????????????????????????
-@???????2022??02??19?????????????????????
+@?????�?2022??02??19??????????�????????�?
 @????????
 @???????????????
 @??    ???http://www.lqist.cn
@@ -21,6 +21,9 @@ int duty2;
 int duty3;
 int duty = 1000;
 double k;
+int k1;
+
+int kick = 0;
 
 int main(void)
 {
@@ -29,95 +32,134 @@ int main(void)
 	SystemClock_Config(); // ???????9???,72M
 	delay_init(72);		  // ??'?????????
 	JTAG_Set(SWD_ENABLE); // ??SWD??? ?????????????SWD??????
-	
 
 	//-----------------------------------------------------------------
-	uart_init(USART_3, 115200);			//??'??????    ????LQMV4???g??  
-	uart_init(USART_2, 115200);			//??'??????    ???????
-    OLED_Init();
-	LED_Init();							//??'??LED	
+	uart_init(USART_3, 115200); //??'??????    ????LQMV4???g??
+	uart_init(USART_2, 115200); //??'??????    ???????
+	OLED_Init();
+	LED_Init(); //??'??LED
 	MotorInit();
-	delay_ms(500);		// ????
+	delay_ms(500); // ????
 	printf("Long Qiu Ke Ji\n");
 	printf("USART3 test,This is USART2.\n");
-    uart_SendBuf(&USART3_Handler, (uint8_t*)"This is USART3!\n");
-    OLED_P8x16Str(10,0,"UART Test");              //?????
-	
-	while(1)
+
+	while (1)
 	{
-		//MotorCtrl3w(0, 1700, 1700);
-		if(receive_oneline3 != 0)
+		uart_SendBuf(&USART3_Handler, (uint8_t *)"This is USART3!\n");
+		OLED_P8x16Str(10, 0, "UART Test"); //?????
+
+		k1 = 1;
+		while (k1 == 1)
 		{
-			k = x_error;
-			uart_SendBuf(&USART3_Handler, (uint8_t*)ReadBuff);
-			x_error = atof((char*)ReadBuff);
-			sprintf(txt, "%f", x_error);
-            OLED_P8x16Str(0, 4, txt); //??????????????????OLED?
-			LED_Ctrl(RVS); 
-            //printf("R:%s\n",ReadBuff);  //????2????????????
-			
-			//
-			x_error = atof((char*)ReadBuff);
-			
-			if (x_error < 1000)
+			// MotorCtrl3w(1200, 0, 0);
+			// LED_Ctrl(RVS);
+			if (receive_oneline3 != 0)
 			{
-				if (k > 1000)
+				k = x_error;
+				uart_SendBuf(&USART3_Handler, (uint8_t *)ReadBuff);
+				x_error = atof((char *)ReadBuff);
+				sprintf(txt, "%f", x_error);
+				OLED_P8x16Str(0, 4, txt); //??????????????????OLED?
+				LED_Ctrl(RVS);
+				// printf("R:%s\n",ReadBuff);  //????2????????????
+
+				//
+				x_error = atof((char *)ReadBuff);
+
+				if (x_error < 1000)
 				{
-					MotorCtrl3w(-600, -600, -600);
-				  delayms(100);
+					if (k > 1000)
+					{
+						MotorCtrl3w(-600, -600, -600);
+						delayms(100);
+					}
+					if (k == 100 && x_error != 100)
+					{
+						MotorCtrl3w(-600, 0, 0);
+						delayms(100);
+					}
+					if (k == 200 && x_error != 200)
+					{
+						MotorCtrl3w(2000, 2000, 2000);
+						// break;
+						delayms(100);
+						x_error = 10000;
+					}
+					if (k <= 1 && k >= -1 && x_error == 100)
+					{
+						MotorCtrl3w(0, 0, 0);
+						delayms(100);
+					}
+					if (x_error < -5) // jin qiu
+					{
+						duty1 = 0;
+						duty2 = 0;
+						duty3 = 0;
+						MotorCtrl3w(duty1, duty2, duty3);
+					}
+
+					else // no jin qiu
+					{
+						if (x_error >= 0 && x_error <= 1) // chao hong qiu zou
+						{
+							duty1 = 800 * x_error + 700;
+							duty2 = -1050 + 1100 * x_error;
+							duty3 = 1000 - 1100 * x_error;
+							MotorCtrl3w(duty1, duty2, duty3);
+						}
+						if (x_error < 0 && x_error >= -1) // chao hong qiu zou
+						{
+							duty1 = 800 * x_error - 800;
+							duty2 = -1050 + 2500 * x_error;
+							duty3 = 1000 - 1200 * x_error;
+							MotorCtrl3w(duty1, duty2, duty3);
+						}
+						if (x_error == 100) // rao qiu zhuan
+						{
+							duty1 = 1200;
+							duty2 = 0;
+							duty3 = -800;
+							MotorCtrl3w(duty1, duty2, duty3);
+						}
+						if (x_error > 200 && x_error <= 201) // chao qiu men zou
+						{
+							duty1 = -900;
+							duty2 = 500;
+							duty3 = 0;
+							MotorCtrl3w(duty1, duty2, duty3);
+						}
+						if (x_error < 200 && x_error >= 199) // chao qiu men zou
+						{
+							duty1 = 900;
+							duty2 = 0;
+							duty3 = -500;
+							MotorCtrl3w(duty1, duty2, duty3);
+						}
+						if (x_error == 200)
+						{
+							k1 = 0;
+						}
+					}
 				}
-				/*if (x_error > 10)
-				{
-					if (x_error > 100)
-					{
-						duty1 = 800 * (x_error-100) + 300;
-				duty2 = -1300;
-				duty3 = 1200;
-				MotorCtrl3w(duty1, duty2, duty3);
-					}
-					else if (x_error < 100)
-					{
-						duty1 = 800 * (x_error-100) - 400;
-				duty2 = -1300;
-				duty3 = 1200;
-				MotorCtrl3w(duty1, duty2, duty3);
-					}
-					duty1 = 0;
-				duty2 = 0;
-				duty3 = 0;
-				MotorCtrl3w(duty1, duty2, duty3);
-				}*/
 				else
 				{
-					if (x_error > 0)
-					{
-						duty1 = 800 * x_error + 700;
-				duty2 = -1650 + 1000 * x_error;
-				duty3 = 1600 - 1000 * x_error;
-				MotorCtrl3w(duty1, duty2, duty3);
-					}
-					else if (x_error < 0)
-					{
-						duty1 = 800 * x_error - 800;
-				duty2 = -1650 + 2500 * x_error;
-				duty3 = 1600 - 1200 * x_error;
-				MotorCtrl3w(duty1, duty2, duty3);
-					}
+					duty1 = 1000;
+					duty2 = 1000;
+					duty3 = 1000;
+					MotorCtrl3w(duty1, duty2, duty3);
+				}
+				receive_oneline3 = 0; //???h???\n???????????????
+			}
+			if (k1 == 0)
+			{
+				break;
 			}
 		}
-			else
-			{
-				duty1 = 1000;
-				duty2 = 1000;
-				duty3 = 1000;
-				MotorCtrl3w(duty1, duty2, duty3);
-			}
-			receive_oneline3=0;         //???h???\n???????????????
-		}            
-		
 
-        //delay_ms(50); 
-		
+		duty1 = 0;
+		duty2 = -5560;
+		duty3 = 5500;
+		MotorCtrl3w(duty1, duty2, duty3);
+		delayms(400);
 	}
 }
-
--- single_color_code_tracking_1.py
+++ single_color_code_tracking_1.py
@@ -15,24 +15,24 @@ import math
 import display
 
 import pyb
-from pyb import UART,LED,Pin
+from pyb import UART, LED, Pin
 
-LEDB = LED(3)                  # 蓝色LED
+LEDB = LED(3)  # 蓝色LED
 
-uart = UART(3, 115200)         # 初始化串口3，波特率115200 --》母板上 UART接口
+uart = UART(3, 115200)  # 初始化串口3，波特率115200 --》母板上 UART接口
 
 # Color Tracking Thresholds (L Min, L Max, A Min, A Max, B Min, B Max)
 # The below thresholds track in general red/green things. You may wish to tune them...
 thresholds = [
     (
-        18,
+        22,
         100,
-        20,
+        41,
         127,
-        0,
+        -128,
         127,
     ),  # generic_red_thresholds -> index is 0 so code == (1 << 0)
-     (65, 99, -74, 12, 41, 53),
+    (65, 99, -74, 12, 41, 53),
 ]  # generic_green_thresholds -> index is 1 so code == (1 << 1)
 # Codes are or'ed together when "merge=True" for "find_blobs".
 
@@ -46,13 +46,15 @@ lcd = display.SPIDisplay()
 
 clock = time.clock()
 
+
 def find_max(blobs):
-    max_size=0
+    max_size = 0
     for blob in blobs:
-        if blob[2]*blob[3] > max_size:
-            max_blob=blob
-            max_size = blob[2]*blob[3]
+        if blob[2] * blob[3] > max_size:
+            max_blob = blob
+            max_size = blob[2] * blob[3]
     return max_blob
+
 
 # Only blobs that with more pixels than "pixel_threshold" and more area than "area_threshold" are
 # returned by "find_blobs" below. Change "pixels_threshold" and "area_threshold" if you change the
@@ -62,8 +64,12 @@ while True:
     clock.tick()
     img = sensor.snapshot()
     x_error = 10000
-    blobs1 = img.find_blobs([thresholds[1]], pixels_threshold=100, area_threshold=100, merge=True)
-    if blobs1:                  # 找到追踪目标
+    x_error_red = 10000
+    # 找红色
+    blobs1 = img.find_blobs(
+        [thresholds[0]], pixels_threshold=100, area_threshold=100, merge=True
+    )
+    if blobs1:  # 找到追踪目标
         blob = find_max(blobs1)  # 提取blobs中面积最大的一个色块blob
         if blob.elongation() > 0.5:
             img.draw_edges(blob.min_corners(), color=(255, 0, 0))
@@ -73,28 +79,54 @@ while True:
         img.draw_rectangle(blob.rect())
         img.draw_cross(blob.cx(), blob.cy())
         # Note - the blob rotation is unique to 0-180 only.
-        img.draw_keypoints([(blob.cx(), blob.cy(), int(math.degrees(blob.rotation())))], size=20)
-        x_error = 2 * (blob.cx()/img.width()) - 1
-        print(blob.h())
-        if blob.h() > 125:
-            x_error = x_error + 100
+        img.draw_keypoints(
+            [(blob.cx(), blob.cy(), int(math.degrees(blob.rotation())))], size=20
+        )
+        x_error = 2 * (blob.cx() / img.width()) - 1
+        x_error_red = x_error
+        print(blob.cy())
+        y_up = blob.y()
+        if blob.cy() > 111:
+            x_error = 100
 
+    # if x_error == 100:
+    # 找绿色
+    blobs2 = img.find_blobs(
+        [thresholds[1]], pixels_threshold=100, area_threshold=100, merge=True
+    )
+    if blobs2:  # 找到追踪目标
+        blob = find_max(blobs2)  # 提取blobs中面积最大的一个色块blob
+        if blob.elongation() > 0.5:
+            img.draw_edges(blob.min_corners(), color=(255, 0, 0))
+            img.draw_line(blob.major_axis_line(), color=(0, 255, 0))
+            img.draw_line(blob.minor_axis_line(), color=(0, 0, 255))
+        # These values are stable all the time.
+        img.draw_rectangle(blob.rect())
+        img.draw_cross(blob.cx(), blob.cy())
+        # Note - the blob rotation is unique to 0-180 only.
+        img.draw_keypoints(
+            [(blob.cx(), blob.cy(), int(math.degrees(blob.rotation())))], size=20
+        )
+        x_error_green = 2 * (blob.cx() / img.width()) - 1
+        # print(abs(x_error_red - x_error_green))
+        if abs(x_error_red - x_error_green) < 0.13:
+            x_error = 200
+        y_down = blob.y() + blob.h()
+        # print(blob.y() + blob.h())
+        # if y_down > y_up:
+        #    x_error = -10
 
+        # print(blob.cx())
+        # print("xy")
+        # print(blob.cy())
+        # 计算中心偏差x_error
 
-        #print(blob.cx())
-        #print("xy")
-        #print(blob.cy())
-                     # 计算中心偏差x_error
+    uart.write(str(x_error) + "\n")  # 将变量通过串口发送出
 
-
-    uart.write(str(x_error) + '\n')                       # 将变量通过串口发送出
-
-    time.sleep_ms(100)                          # 延时0.5秒
-    LEDB.toggle()
-    if uart.any():                              # 如果接收到任何消息
+    time.sleep_ms(100)  # 延时0.5秒
+    # LEDB.toggle()
+    if uart.any():  # 如果接收到任何消息
         receive = uart.read().decode().strip()  # 将接收到的消息提取出来
-        #print(receive)  #在串行终端 打印接收到的信息
-    #lcd.write(img)
-    #print(clock.fps())
-
-
+        # print(receive)  #在串行终端 打印接收到的信息
+    # lcd.write(img)
+    # print(clock.fps())
