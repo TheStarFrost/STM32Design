4d1865e0d6cf37ab74b87468fe92c5b2f36da369
TheStarFrost <138985340+TheStarFrost@users.noreply.github.com>
2024-08-29 16:16:52 +0800

    踢球对战版本代码最终版,附日志更新

--- ColorTracking.py
+++ ColorTracking.py
@@ -25,15 +25,14 @@ uart = UART(3, 115200)         # 初始化串口3，波特率115200 --》母板�
 # The below thresholds track in general red/green things. You may wish to tune them...
 thresholds = [
     (
-        2,
-        43,
-        17,
-        -18,
-        32,
-        -4,
+        22,
+        100,
+        41,
+        127,
+        -128,
+        127,
     ),  # generic_red_thresholds -> index is 0 so code == (1 << 0)
-    (29, 87, -106, -17, 19, 52),
-    (0, 38, 26, 127, -26, 30)
+    (40, 99, -74, 12, 42, 73),
 ]  # generic_green_thresholds -> index is 1 so code == (1 << 1)
 # Codes are or'ed together when "merge=True" for "find_blobs".
 
@@ -47,20 +46,12 @@ lcd = display.SPIDisplay()
 
 clock = time.clock()
 
-def find_low(blobs):
-    max_cy=0
-    for blob in blobs:
-        if blob.cy() > max_cy:
-            max_blob=blob
-            max_cy = blob.cy()
-    return max_blob
-
 def find_max(blobs):
-    max_size = 0
+    max_size=0
     for blob in blobs:
-        if blob[2] * blob[3] > max_size:
-            max_blob = blob
-            max_size = blob[2] * blob[3]
+        if blob[2]*blob[3] > max_size:
+            max_blob=blob
+            max_size = blob[2]*blob[3]
     return max_blob
 
 # Only blobs that with more pixels than "pixel_threshold" and more area than "area_threshold" are
@@ -72,73 +63,60 @@ while True:
     img = sensor.snapshot()
     x_error = 10000
     x_error_red = 10000
-    #找黑色
-    blobs1 = img.find_blobs([thresholds[0]], roi = [0, 50, 160, 140], pixels_threshold=100, area_threshold=100, merge=True)
+    #找红色
+    blobs1 = img.find_blobs([thresholds[0]], pixels_threshold=0, area_threshold=0, merge=True)
     if blobs1:                  # 找到追踪目标
-        blob = find_low(blobs1)  # 提取blobs中面积最大的一个色块blob
+        blob = find_max(blobs1)  # 提取blobs中面积最大的一个色块blob
         if blob.elongation() > 0.5:
             img.draw_edges(blob.min_corners(), color=(255, 0, 0))
             img.draw_line(blob.major_axis_line(), color=(0, 255, 0))
             img.draw_line(blob.minor_axis_line(), color=(0, 0, 255))
-            img.draw_rectangle((0, 50, 160, 140),color=(255, 0, 0))       # 根据色块blob位置画红色框
-            img.draw_cross(80, 120,color=(0, 0, 255))  # 根据色块位置在中心画蓝色十字
         # These values are stable all the time.
         img.draw_rectangle(blob.rect())
         img.draw_cross(blob.cx(), blob.cy())
         # Note - the blob rotation is unique to 0-180 only.
         img.draw_keypoints([(blob.cx(), blob.cy(), int(math.degrees(blob.rotation())))], size=20)
-        x_error = 2 * (blob.cx()/img.width()) - 1 - 0.2
+        x_error = 2 * (blob.cx()/img.width()) - 1
         x_error_red = x_error
-        print(x_error)
+        print(blob.cy())
         y_up = blob.y()
-        if blob.cy() < 90:
+        if blob.cy() > 62:
+            x_error = x_error + 10
+        if blob.cy() > 115:
             x_error = 100
 
 
-    #找绿色
-        blobs2 = img.find_blobs([thresholds[1]], roi = [0, 50, 160, 140], pixels_threshold=100, area_threshold=100, merge=True)
-        if blobs2:                  # 找到追踪目标
-            blob = find_max(blobs2)  # 提取blobs中面积最大的一个色块blob
-            if blob.elongation() > 0.5:
-                img.draw_edges(blob.min_corners(), color=(255, 0, 0))
-                img.draw_line(blob.major_axis_line(), color=(0, 255, 0))
-                img.draw_line(blob.minor_axis_line(), color=(0, 0, 255))
-                img.draw_rectangle((0, 50, 160, 140),color=(255, 0, 0))       # 根据色块blob位置画红色框
-                img.draw_cross(80, 120,color=(0, 0, 255))  # 根据色块位置在中心画蓝色十字
-            # These values are stable all the time.
-            img.draw_rectangle(blob.rect())
-            img.draw_cross(blob.cx(), blob.cy())
-            # Note - the blob rotation is unique to 0-180 only.
-            img.draw_keypoints([(blob.cx(), blob.cy(), int(math.degrees(blob.rotation())))], size=20)
-            x_error = 2 * (blob.cx()/img.width()) - 1 - 0.2
-            x_error_red = x_error
-            #print(blob.area())
-            y_up = blob.y()
-            if blob.area() > 8200:
-                x_error = 500
-                #print("OKKKKKKKKKKKLKKK")
 
+    #if x_error == 100:
         #找绿色
-    blobs3 = img.find_blobs([thresholds[2]], roi = [0, 50, 160, 140], pixels_threshold=100, area_threshold=100, merge=True)
-    if blobs3:                  # 找到追踪目标
-        blob = find_max(blobs3)  # 提取blobs中面积最大的一个色块blob
+    blobs2 = img.find_blobs([thresholds[1]], pixels_threshold=100, area_threshold=100, merge=True)
+    if blobs2:                  # 找到追踪目标
+        blob = find_max(blobs2)  # 提取blobs中面积最大的一个色块blob
         if blob.elongation() > 0.5:
             img.draw_edges(blob.min_corners(), color=(255, 0, 0))
             img.draw_line(blob.major_axis_line(), color=(0, 255, 0))
             img.draw_line(blob.minor_axis_line(), color=(0, 0, 255))
-            img.draw_rectangle((0, 50, 160, 140),color=(255, 0, 0))       # 根据色块blob位置画红色框
-            img.draw_cross(80, 120,color=(0, 0, 255))  # 根据色块位置在中心画蓝色十字
         # These values are stable all the time.
         img.draw_rectangle(blob.rect())
         img.draw_cross(blob.cx(), blob.cy())
         # Note - the blob rotation is unique to 0-180 only.
         img.draw_keypoints([(blob.cx(), blob.cy(), int(math.degrees(blob.rotation())))], size=20)
-        x_error = 2 * (blob.cx()/img.width()) - 1 - 0.2
-        x_error_red = x_error
+        x_error_green = 2 * (blob.cx()/img.width()) - 1
+        print(blob.cy())
         print(blob.area())
-        y_up = blob.y()
-        if blob.area() > 6000:
-            x_error = 800
+        if x_error == 100 and x_error_green < 0 and abs(x_error_red - x_error_green) >= 0.13:
+            x_error = 300 #公转方向为逆时针
+
+        if abs(x_error_red - x_error_green) < 0.13 and blob.area() <= 4900:
+            x_error = 200
+        if abs(x_error_red - x_error_green) < 0.13 and blob.area() > 4900:
+            x_error = 250
+        if blob.cy() > 39 and blob.area() > 10000:
+            x_error = 251
+        y_down = blob.y() + blob.h()
+            #print(blob.y() + blob.h())
+            #if y_down > y_up:
+            #    x_error = -10
 
 
 
@@ -154,12 +132,11 @@ while True:
 
     uart.write(str(x_error) + '\n')                       # 将变量通过串口发送出
 
-    time.sleep_ms(100)                          # 延时0.5秒
+    time.sleep_ms(97)                          # 延时0.5秒
     #LEDB.toggle()
     if uart.any():                              # 如果接收到任何消息
         receive = uart.read().decode().strip()  # 将接收到的消息提取出来
         #print(receive)  #在串行终端 打印接收到的信息
     #lcd.write(img)
     #print(clock.fps())
-
 
--- Project/USER/main.c
+++ Project/USER/main.c
@@ -12,46 +12,136 @@
 QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ*/
 
 // ??????????l?
-#include "include.h"
+/*#include "include.h"
 
+double x_error = 0;
+char txt[32];
+int duty1;
+int duty2;
+int duty3;
+int duty = 1000;
+double k;
 
-void straight(int duty1, int duty2, int duty3, char* txt)//the car go staight
+int main(void)
 {
-	if (duty1 < PWM_DUTY_MAX) // ?????10000,??7200
+	//-----------------------????'??????----------------------------
+	HAL_Init();			  // ??'??HAL??
+	SystemClock_Config(); // ???????9???,72M
+	delay_init(72);		  // ??'?????????
+	JTAG_Set(SWD_ENABLE); // ??SWD??? ?????????????SWD??????
+	
+
+	//-----------------------------------------------------------------
+	uart_init(USART_3, 115200);			//??'??????    ????LQMV4???g??  
+	uart_init(USART_2, 115200);			//??'??????    ???????
+    OLED_Init();
+	LED_Init();							//??'??LED	
+	MotorInit();
+	delay_ms(500);		// ????
+	printf("Long Qiu Ke Ji\n");
+	printf("USART3 test,This is USART2.\n");
+    uart_SendBuf(&USART3_Handler, (uint8_t*)"This is USART3!\n");
+    OLED_P8x16Str(10,0,"UART Test");              //?????
+	
+	while(1)
 	{
-	duty1 = 0;
-	duty2 = -2450;
-	duty3 = 2400; 
-	MotorCtrl3w(duty1,duty2, duty3);
-	//sprintf(txt, "PWM: duty1=%2d,duty2=%2d,duty3=%2d", duty1, duty2, duty3);
-	//OLED_P6x8Str(10, 5, txt);
-	}
-}
-void left(int duty1, int duty2, int duty3, char* txt)//the car turn left
-{
-	if (duty1 < PWM_DUTY_MAX) // ?????10000,??7200
-	{
-	duty1 = -600;
+		//MotorCtrl3w(0, 1700, 1700);
+		if(receive_oneline3 != 0)
+		{
+			k = x_error;
+			uart_SendBuf(&USART3_Handler, (uint8_t*)ReadBuff);
+			x_error = atof((char*)ReadBuff);
+			sprintf(txt, "%f", x_error);
+            OLED_P8x16Str(0, 4, txt); //??????????????????OLED?
+			LED_Ctrl(RVS); 
+            //printf("R:%s\n",ReadBuff);  //????2????????????
+			
+			//
+			x_error = atof((char*)ReadBuff);
+			
+			if (x_error < 1000)
+			{
+				if (k > 1000)
+				{
+					MotorCtrl3w(-600, -600, -600);
+				  delayms(100);
+				}
+				if (x_error > 10)
+				{
+					if (x_error > 100)
+					{
+						
+						duty1 = 800 * (x_error-100) + 300;
+				duty2 = -1300;
+				duty3 = 1200;
+				MotorCtrl3w(duty1, duty2, duty3);
+						duty1 = -600;
 	duty2 = -1400;
 	duty3 = 400;
 	MotorCtrl3w(duty1,duty2, duty3);
-	//sprintf(txt, "PWM: duty1=%2d,duty2=%2d,duty3=%2d", duty1, duty2, duty3);
-	//OLED_P6x8Str(10, 5, txt);
-	}
-}
-
-void right(int duty1, int duty2, int duty3, char* txt)//the car turn right
-{
-	if (duty1 < PWM_DUTY_MAX) // ?????10000,??7200
-	{
-	duty1 = 600;
-	duty2 = -500;
-	duty3 = 1300;
+					}
+					else if (x_error < 100)
+					{
+						
+						duty1 = 800 * (x_error-100) - 400;
+				duty2 = -1300;
+				duty3 = 1200;
+				MotorCtrl3w(duty1, duty2, duty3);
+						duty1 = -600;
+	duty2 = -1400;
+	duty3 = 400;
 	MotorCtrl3w(duty1,duty2, duty3);
-	//sprintf(txt, "PWM: duty1=%2d,duty2=%2d,duty3=%2d", duty1, duty2, duty3);
-	//OLED_P6x8Str(10, 5, txt);
+					}
+				}
+				else
+				{
+					if (x_error > 0)
+					{
+						duty1 = 800 * x_error + 700;
+				duty2 = -1650 + 1000 * x_error;
+				duty3 = 1600 - 1000 * x_error;
+				MotorCtrl3w(duty1, duty2, duty3);
+					}
+					else if (x_error < 0)
+					{
+						duty1 = 800 * x_error - 800;
+				duty2 = -1650 + 2500 * x_error;
+				duty3 = 1600 - 1200 * x_error;
+				MotorCtrl3w(duty1, duty2, duty3);
+					}
+			}
+		}
+			else
+			{
+				duty1 = 1000;
+				duty2 = 1000;
+				duty3 = 1000;
+				MotorCtrl3w(duty1, duty2, duty3);
+			}
+			receive_oneline3=0;         //???h???\n???????????????
+		}            
+		
+
+        //delay_ms(50); 
+		
 	}
-}
+}*/
+/*LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL
+@??    ?????????
+@E-mail  ??chiusir@163.com
+@????IDE ??KEIL5.25.3???????
+@'??????????????????????????????
+@?????£?2022??02??19??????????£????????°?
+@????????
+@???????????????
+@??    ???http://www.lqist.cn
+@????????http://longqiu.taobao.com
+@???????V1.0 ???????????'????????????
+QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ*/
+
+// ??????????l?
+#include "include.h"
+
 double x_error = 0;
 char txt[32];
 int duty1;
@@ -60,7 +150,6 @@ int duty3;
 int duty = 1000;
 double k;
 int k1;
-int k2 = 1;
 	
 int kick = 0;
 
@@ -82,17 +171,18 @@ int main(void)
 	delay_ms(500);		// ????
 	printf("Long Qiu Ke Ji\n");
 	printf("USART3 test,This is USART2.\n");
-    //MotorCtrl3w(0, 0, 1200);
-	//delayms(3000);
+  MotorCtrl3w(0, -4600, 4000);
+			  delayms(100);
 	
 	
 	while(1)
 	{
+		//MotorCtrl3w(0, -1500, 1000);
+		
 		uart_SendBuf(&USART3_Handler, (uint8_t*)"This is USART3!\n");
     OLED_P8x16Str(10,0,"UART Test");              //?????
 		
 		k1 = 1;
-		k2 = 1;
 	while(k1 == 1)
 	{
 		//MotorCtrl3w(1200, 0, 0);
@@ -107,84 +197,166 @@ int main(void)
 			LED_Ctrl(RVS); 
             //printf("R:%s\n",ReadBuff);  //????2????????????
 			
-			//
-			x_error = atof((char*)ReadBuff);
-			
 			if (x_error < 1000)
 			{
-				  
-				
-				
-					if (x_error >= 0.15 && x_error <= 0.8)//chao hong qiu zou
-					{
-						right(duty1, duty2, duty3, txt);
+				if (k > 1000)
+				{
+					MotorCtrl3w(-600, -600, -600);
+				  delayms(100);
+				}
+				if (k == 100 && x_error != 100)
+				{
+					MotorCtrl3w(-2000, 0, 0);
+				  delayms(100);
+				}
+				if (k == 200 && x_error != 200)
+				{
+					MotorCtrl3w(2000, 2000, 2000);
+					//break;
+					delayms(100);
+					x_error = 10000;
+					
+				}
+				if (k <= 1 && k >= -1 && x_error == 100)
+				{
+					MotorCtrl3w(0, 0, 0);
+				  delayms(100);
+				}
+				if (x_error < -5) //jin qiu
+				{
+				duty1 = 0;
+				duty2 = 0;
+				duty3 = 0;
+				MotorCtrl3w(duty1, duty2, duty3);
 					}
-					if (x_error < -0.2 && x_error >= -1.2)//chao hong qiu zou
+				
+				else  //no jin qiu
+				{
+					if (x_error >= 0 && x_error <= 1)//chao hong qiu zou
 					{
-						left(duty1, duty2, duty3, txt);
+						duty1 = 800 * x_error + 800;
+				duty2 = -2350 + 1100 * x_error;
+				duty3 = 2300 - 1100 * x_error;
+				MotorCtrl3w(duty1, duty2, duty3);
+					}
+					if (x_error < 0 && x_error >= -1)//chao hong qiu zou
+					{
+						duty1 = 800 * x_error - 900;
+				duty2 = -2350 + 2500 * x_error;
+				duty3 = 2300 - 1200 * x_error;
+				MotorCtrl3w(duty1, duty2, duty3);
 					}
 					
-					if (x_error >= 0 && x_error <= 0.15)//chao hong qiu zou
+					if (x_error >= 10 && x_error <= 11)//chao hong qiu zou
 					{
-						straight(duty1, duty2, duty3, txt);
+						duty1 = 800 * (x_error-10) + 700;
+				duty2 = -950 + 1100 * (x_error-10);
+				duty3 = 900 - 1100 * (x_error-10);
+				MotorCtrl3w(duty1, duty2, duty3);
 					}
-					if (x_error < 0 && x_error >= -0.2)//chao hong qiu zou
+					if (x_error < 10 && x_error >= 9)//chao hong qiu zou
 					{
-						straight(duty1, duty2, duty3, txt);
+						duty1 = 800 * (x_error-10) - 800;
+				duty2 = -1050 + 2500 * (x_error-10);
+				duty3 = 1000 - 1200 * (x_error-10);
+				MotorCtrl3w(duty1, duty2, duty3);
 					}
+					
 					if (x_error == 100)//rao qiu zhuan
 					{
-				duty1 = -1000;
-				duty2 = -1000;
-				duty3 = -1000;
+				duty1 = 1800;
+				duty2 = 0;
+				duty3 = -700;
 				MotorCtrl3w(duty1, duty2, duty3);
 					}
-					if (x_error == 500)
+					if (x_error == 300)//rao qiu zhuan  (ni shi zhen)
+					{
+				duty1 = -1500;
+				duty2 = 0;
+				duty3 = -600;
+				MotorCtrl3w(duty1, duty2, duty3);
+					}
+					if (x_error > 200 && x_error <= 201)//chao qiu men zou
+					{
+						duty1 = -900;
+				duty2 = 500;
+				duty3 = 0;
+				MotorCtrl3w(duty1, duty2, duty3);
+					} 
+					if (x_error < 200 && x_error >= 199)//chao qiu men zou
+					{
+						duty1 = 900;
+				duty2 = 0;
+				duty3 = -500;
+				MotorCtrl3w(duty1, duty2, duty3);
+					}
+					if (x_error == 200)
 					{
 						k1 = 0;
+						//duty1 = 0;
+				//duty2 = -3600;
+				//duty3 = 2000;
+						MotorCtrl3w(duty1, duty2, duty3);
 					}
-					if (x_error == 800)
+					if (x_error == 250)
 					{
-						MotorCtrl3w(0, 0, 0);
-						k2 = 0;
+						duty1 = 0;
+				    duty2 = -2660;
+				duty3 = 2500;
+						MotorCtrl3w(duty1, duty2, duty3);
 					}
-				}
-					
+					if (x_error == 251)
+					{
+						k1 = 5;
+					}
+			}
+		}
 			else
 			{
-				duty1 = -1000;
-				duty2 = -1000;
-				duty3 = -1000;
+				duty1 = 1000;
+				duty2 = 1000;
+				duty3 = 1000;
 				MotorCtrl3w(duty1, duty2, duty3);
-			}
-			if (k1 == 0)
-			{
-				break;
 			}
 			receive_oneline3=0;         //???h???\n???????????????
 		}
-    		
-		
+    if (k1 != 1)
+		{
+			break;
+		}			
+	}		
+		if (k1 == 0)
+		{
+			MotorCtrl3w(-1600, 0, 0);
+				  delayms(165);
+			duty1 = 0;
+				duty2 = -4600;
+				duty3 = 4000;
+						MotorCtrl3w(duty1, duty2, duty3);
+						delayms(1200);
+			MotorCtrl3w(0, 0, 0);
+			 delayms(800);
+		}
 
-		
+     if (k1 == 5)
+		 {
+			 duty1 = 0;
+				duty2 = 2500;
+				duty3 = -2600;
+						MotorCtrl3w(duty1, duty2, duty3);
+						delayms(500);
+			 duty1 = 1200;
+				duty2 = 1200;
+				duty3 = 1200;
+						MotorCtrl3w(duty1, duty2, duty3);
+			 delayms(360);
+			 
+		 }
 	
 	
 		
-}
-	if (k2 == 1)
-  {
-	MotorCtrl3w(1700, 0, 0);
-	delayms(2150);
-	MotorCtrl3w(1000, 1000, 1000);
-delayms(860);
-MotorCtrl3w(0, 0, 0);
-delayms(400);
 	}
-	else
-	{
-		MotorCtrl3w(0, 0, 0);
-		delayms(10000);
-	}
-}	
+	 
+						
 
 }
